ce48c5dc6db1136c20fd5ef8bdc3d11b65d72a9c
EzraBK <35712086+EzraBK@users.noreply.github.com>
2018-01-26 13:49:06 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+/**
+  Author: Ezra Bailey-Kelly
+  Date: 01/25/2018
+  Description: Hello World in C 
+*/ 
+
+#include <stdio.h> 
+ 
+int main(int argc, char **argv) {
+  printf("Ezra Bailey-Kelly\n");
+  return 0;
+}
